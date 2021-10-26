44e105c4bb25849e4df13cee64063891f687017e
Дмитрий <90109742+smirnovdm2107@users.noreply.github.com>
2021-10-26 23:37:03 +0300

    pLlv.0

--- LearnLenguage/src/MainWindow.java
+++ LearnLenguage/src/MainWindow.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class MainWindow {
+    public MainWindow() {
+
+    }
+
+    public void createWindow() {
+        JFrame frame = new JFrame("Hello, world!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
 }
