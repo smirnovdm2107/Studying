1a7868f874d8e6be14e9261f4a2f258b509febb2
Дмитрий <90109742+smirnovdm2107@users.noreply.github.com>
2021-10-29 19:03:02 +0300

    LLV0.5

---
+++ LearnLenguage/src/LearnLenguage.java
@@ -0,0 +1,8 @@
+import com.sun.tools.javac.Main;
+
+public class LearnLenguage {
+
+    public static void main(String[] args) {
+       MainWindow win = new MainWindow();
+    }
+}
--- LearnLenguage/src/MainWindow.java
+++ LearnLenguage/src/MainWindow.java
@@ -1,14 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainWindow {
-    public MainWindow() {
+public class MainWindow extends JFrame {
 
-    }
 
-    public void createWindow() {
-        JFrame frame = new JFrame("Hello, world!");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
+        public MainWindow() {
+            super("LL");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setPreferredSize(new Dimension(800, 600));
+            Container contentPane = getContentPane();
+            setLocationRelativeTo(null);
+            pack();
+            setVisible(true);
+            Container container = getLayeredPane();
 
+            // Making settings button (in progress)
+            JButton settings = new JButton("settings");
+            settings.setBounds(10, 10, 100, 20);
+            container.add(settings);
+
+
+            // Making start button
+            JButton start = new JButton("START");
+            start.setBounds(10, 40, 100, 20);
+            start.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JLabel word = new JLabel("Fuck");
+                    word.setBounds(350, 250, 80, 40);
+
+                    container.add(word);
+                }
+            });
+            container.add(start);
+
+        }
 }
